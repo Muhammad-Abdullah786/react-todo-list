29be1de2440ad3c1c4eb2ae2c7f76383672df1b5
Muhammad Abdullah <abdulx125@gmail.com>
2024-07-20 13:06:28 -0700

    working  left in todo context

--- TO-Do-List/src/contexts/TodoContext.js
+++ TO-Do-List/src/contexts/TodoContext.js
@@ -1,1 +1,20 @@
-o
+import { useContext, createContext } from 'react'
+
+export const TodosContext = createContext({
+    todo : [
+        {
+            id : 1,
+            title : "todo msg",
+            title : "todo msg",
+        },
+        {},
+        {},
+    ]
+});
+
+export const TodoProvider = TodosContext.Provider
+
+//hook 
+export const useTodo = () => {
+    return useContext(TodosContext)
+}
