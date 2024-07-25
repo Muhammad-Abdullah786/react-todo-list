639c8b1559189cafb6bfbd8e6cf5a2d4ad3ae574
Muhammad Abdullah <abdulx125@gmail.com>
2024-07-25 23:48:48 +0500

    added addtodo, updateTodo fucntion

--- TO-Do-List/src/App.jsx
+++ TO-Do-List/src/App.jsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
+import { useTodo, TodoProvider, TodosContext } from "./contexts/index";
 
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState([]);
 
+  const addTodo = (title) => {
+    // Add Todo Logic here
+    // here i have make a new array in prev i have previous value of array
+    // in new array create a new object
+    setTodo((prev) => [{ id: Date.now(), ...title }, ...prev]);
+  };
+   
+  const updateTodo = (id, title) => {
+    setTodo((prev) => prev.map((prevTodo) => prevTodo.id === id ? title: prevTodo))
+  }
   
-
-
+  const deleteTodo = (id) => {
+    // one way is to use map and found what id is going to delete
+    // in prev.map we are getting each todo meaning prev is opject but 
+    //prevTodo is each todo
+    setTodo((prev)=> prev.map((prevTodo) => prevTodo.id === id ? : ))
+  };
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <TodoProvider
+      value={{ todo, addTodo, deleteTodo, updateTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
 
